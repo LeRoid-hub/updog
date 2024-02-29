a93f62f03628e5cc49060d60005a46e604f62e13
Jan Barfuss <janbar2001@gmail.com>
2024-02-29 11:22:07 +0100

    dummy test classes

--- tests/service_test.py
+++ tests/service_test.py
@@ -1,3 +1,7 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from updog import Service
+
+class TestService(unittest.TestCase):
+    def test_dummy(self):
+        self.assertTrue(True)
--- updog/__init__.py
+++ updog/__init__.py
@@ -1,6 +1,6 @@
 from .updog import Updog
 from .logger import Logger
-from .Service import Service
+from .service import Service
 from .server import Server
 from .builder import PostgreSQLBuilder, HTTPGetBuilder
 from .notify import Notify
